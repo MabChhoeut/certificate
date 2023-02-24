8d89b7576ceb950cc4a905652956d9b8f7022020
MabChhoeut <chhoeutmab@gmail.com>
2023-02-24 22:32:07 +0700

    add more security before going to index.php

---
+++ footer.php
@@ -0,0 +1,51 @@
+<div>
+    <div class="End" style="background-color:  rgb(12, 135, 218); margin-top: 20px;">
+        <div class="row">
+            <div class="col-lg-4  text-white " style="font-size: 20px;font-family: Khmer OS Muol Light; ">
+                <p style="padding-left: 100px;padding-top: 20px; position: absolute;">សាលាប៊ែលធីអន្តរជាតិ  </p>
+                <p
+                    style="font-size: 18px ;padding-left: 100px;padding-top: 50px; position: absolute; font-family: Georgia, 'Times New Roman', Times, serif;">
+                    BELTIE INTERNASONL SCHOOL
+                </p>
+                <img src="imag/logo1.png" alt=""
+                    style="width:20% ;margin-top:20px ; padding-left: 20px;">
+                <p style="font-family:Khmer OS Battambang; font-size: 12px;padding-left: 30px; ">
+                    <br> - ដើរ 3 នាទីពី សារមន្ទីរប្រល័យពូជសាសន៍ទួលស្លែង<br>
+                    <br>- អាស័យដ្ឋាន ផ្ទះលេខ 21 ផ្លូវ 360 សង្កាត់បឹងកេងកង 3 បឹងកេងកង ភ្នំពេញ 12304 <br>
+                    . ម៉ោង ៖
+                    <br> បើក 7:30 បិទនៅម៉ោង 8:30 យប់ <br>
+                    <br> ទូរស័ព្ទ ៖ ០៧៨ ៥៥៥ ៥០៧ <br>
+                </p>
+            </div>
+            <div class="col-lg-4  text-white "
+                style="padding-left: 50px ; font-family: Khmer OS Battambang;padding-top: 20px;">
+                <h1 style="font-size: 20px;padding-left: 20px;"> <b> Menu </b></h1>
+                <p class="text-white">
+                    <a href="Index.html" style="color:white ;font-size: 12px;"> <br> -ផ្ទះ<br></a>
+                    <a href="index13.html" style="color:white ;font-size: 12px;"><br> -អំពីពួកយើង<br></a>
+                    <a href="https://kh.linkedin.com/in/beltei-international-university-cambodia-9017a11b1"
+                        style="color:white ;font-size: 12px;"><br> - ប្រព័ន្ធផ្សព្វផ្សាយនិងការបោះពុម្ភផ្សាយ <br></a>
+                    <a href="https://kh.linkedin.com/in/beltei-international-university-cambodia-9017a11b1"
+                        style="color:white ;font-size: 12px;"><br> -ទំនាក់ទំនងមកពួកយើង <br></a>
+                </p>
+            </div>
+            <div class="col-lg-4  text-white" style="font-family: Khmer OS Battambang; ">
+
+                <h1 style="font-size: 20px;padding-top: 20px;padding-left: 10px;">
+                    <b>Social Media</b>
+
+                </h1>
+                <div> <a href="https://web.facebook.com/belteiuniversity/?_rdc=1&_rdr" class="fa fa-facebook"></a>
+                    <a href="https://kh.linkedin.com/in/beltei-international-university-cambodia-9017a11b1"
+                        class="fa fa-twitter"></a>
+                    <a href="https://www.google.com/search?gs_ssp=eJwFwVEOQDAMAND4FXfgx7c2szFHcAnZlqKJVDIl3N57ZdVtHWJ-1O6LQDG18BoEbxFX52NwBvwEb3IWHPh-GEdMsae5jnQoccOilCUonxKO5hZ-KF-s3w8hXBrr&q=beltei+international+university&oq=&aqs=chrome.0.46i39i175i199i362j35i39i362l7.1437703j0j7&sourceid=chrome&ie=UTF-8"
+                        class="fa fa-google"></a>
+                    <a href="https://www.youtube.com/results?search_query=beltie+university" class="fa fa-youtube"></a>
+                </div>
+                <p style="margin-top:20px ;font-size: 12px;">
+                    រក្សាសិទ្ធិ echo "&copy;" 2009 សកលវិទ្យាល័យប៊ែលធីអន្តរជាតិ  រក្សាសិទ្ធិគ្រប់យ៉ាង។ ឧបត្ថម្ភដោយ AngkorDev
+                </p>
+            </div>
+        </div>
+    </div>
+  </div>
